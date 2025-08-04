a397b7257fbb98c3b895382fc7d0d283b928c7a1
imdebamrita <imdebamrita@gmail.com>
2025-08-04 12:31:36 +0530

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+// eslint.config.ts
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends(
+    "plugin:@next/next/recommended", // Use this instead of "next/core-web-vitals" in flat config
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended" // If using TS
+  ),
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
+
+  {
+    rules: {
+      // Optional: tweak rules
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // async redirects() {
+  //   return [
+  //     // Basic redirect
+  //     {
+  //       source: "/links/:path*",
+  //       destination: "/", // any link
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
+
+export default nextConfig;
