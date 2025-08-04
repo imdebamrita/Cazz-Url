e30e067b3193076d1e85d21cac39ab8aff41905b
imdebamrita <imdebamrita@gmail.com>
2025-08-04 12:33:40 +0530

    clerk auth setup

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,12 @@
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs'
+
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <SignedIn>{children}</SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+        </>
+    )
+}
