482228a093450ef4893ecc47c2566d1c189b4274
imdebamrita <imdebamrita@gmail.com>
2025-09-11 02:53:21 +0530

    tried to add some theme changing animation but found little complex for manage transitions at multiple levels.... this work if disableTransitionOnChange is desabled

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -16,8 +16,8 @@ export function ThemeToggle() {
             onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
             className="h-8 w-8 p-0"
         >
-            <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className="h-4 w-4 transition-transform duration-700 rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-4 w-4 transition-transform duration-700 rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
         </Button>
     )
